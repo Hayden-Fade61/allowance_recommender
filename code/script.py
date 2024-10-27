173bd059dc18704588eac55e534d463b8e408d67
Hayden-Fade61 <63241537+Hayden-Fade61@users.noreply.github.com>
2024-10-27 16:09:08 +0200

    Changed focus of first script to purely analyzing raw data and generalised stationarity tests into single function

--- code/script.py
+++ code/script.py
@@ -1,29 +1,41 @@
 import pandas as pd
 import seaborn as sns
-from statsmodels.graphics.tsaplots import plot_acf
-from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.stattools import adfuller, kpss
 import matplotlib.pyplot as plt
 from os.path import relpath
 
-# Augmented Dickney-Fuller test for stationarity
+# Augmented Dickey-Fuller test for stationarity
 # Taken from: https://www.statsmodels.org/stable/examples/notebooks/generated/stationarity_detrending_adf_kpss.html#Stationarity-and-detrending-(ADF/KPSS)
-def adf_test(time_series):
-    print("Dickey-Fuller Test Results:")
-    dftest = adfuller(time_series, autolag="AIC")
-    dfoutput = pd.Series(
-        dftest[0:4],
-        index=[
-            "Test Statistic",
-            "p-value",
-            "#Lags Used",
+
+TEST_BASE_INDEX = [ "Test Statistic", "p-value", "#Lags Used"]
+
+TEST_EXTRA_INDEX = {
+    "adf": [
             "Number of Observations Used",
-        ],
+        ]
+}
+
+def stationarity_test(time_series: pd.Series, test: str):
+    index = TEST_BASE_INDEX + TEST_EXTRA_INDEX.get(test.lower(), [])
+    index[0] += f": {test.upper()}"
+    index_len = len(index)
+    result = adfuller(time_series, autolag="AIC") if test.lower() == "adf" else  kpss(time_series)
+    output = pd.Series(
+        result[0:index_len],
+        index=index
     )
-    for key, value in dftest[4].items():
-        dfoutput["Critical Value (%s)" % key] = value
-    print(dfoutput + "\n")
+    for key, value in result[index_len].items():
+        output[f"Critical Value ({key})"] = value
+    return output
+
+def stationarity_tests(time_series):
+    adf_res = stationarity_test(time_series, "adf")
+    kpss_res = stationarity_test(time_series, "kpss")
+    return pd.concat([adf_res, kpss_res])
 
 def main():
+    IMG_OUTPUT_DIRECTORY = "/img/"
     # Set output styling
     pd.options.display.max_rows = 400
     sns.set_theme(style="whitegrid")
@@ -34,8 +46,10 @@ def main():
                 "Canberra", "Australia"]
 
     # Read raw CPI figures from Qx 19xx onwards
+    input_file =  "data/640101.xlsx"
+    print(f"Reading {input_file}...")
     cpi_data = pd.read_excel(
-        relpath("data/640101.xlsx"), 
+        io= input_file, 
         sheet_name= "Data1",
         index_col= 0,
         header= None,
@@ -43,20 +57,14 @@ def main():
         usecols= "A:J", 
         names= col_labels
     )
-
-    print(cpi_data.head())
-    print(cpi_data.shape)
-    # Test raw data for stationarity
-    adf_test(cpi_data['Australia'])
-    # Produce table showing raw data results
-
-    # Apply first difference to all CPI cols 
-    for label in col_labels[1:]:
-        cpi_data[label] = cpi_data[label].diff()
-
-    print(cpi_data.head())
-    print(cpi_data.shape)
-
-    # Test 1st differences for stationarity
+    print("Read done, conducting tests for stationarity...")
     
+    # Test all cols for stationarity
+    stationarity_results = []
+    for col in col_labels[1:]:
+        print(f"Testing {col}...")
+        stationarity_results.append(stationarity_tests(cpi_data[col]))
+    print("Tests complete. Displaying results...\n")
+    # results = pd.DataFrame(stationarity_results, index=col_labels[1:]) 
+    print(results)
 main()
